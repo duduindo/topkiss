a9c2f728afb1e00d38e92c7519e3572a3942963d
unknown <eduardo.paixao@FWS-3325.foster.srv.br>
2015-11-26 16:25:59 -0200

    Tutorial Click

--- AprendendoReact/react/js/script.js
+++ AprendendoReact/react/js/script.js
@@ -1,10 +1,23 @@
 
-
-
-
+//Tutorial 1
 //ReactDOM.render( <h1>110</h1>, document.getElementById("root")	 );
 
-ReactDOM.render( 
-	<h1>1000</h1>,
-	document.getElementById("root")	 
-);
+//Tutorial 2
+//Função/Classe
+// var CommentBox = React.createClass({
+// 	render: function() {		
+// 		return (
+// 			<div className="CommentBox" id="IDaqui">
+// 				Olá, mundo! Eu sou um CommentBox. {this.props.author}
+// 			</div>				
+// 			);
+// 	}
+// });
+// ReactDOM.render( <CommentBox author="Dudu" />, document.getElementById("root") );
+
+
+
+
+//Tutorial Click
+//Parei aqui: https://facebook.github.io/react/docs/interactivity-and-dynamic-uis.html
+
